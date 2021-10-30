46247f547d93871efc841e3ee13b9675e0dfcaaf
Davinder <66485246+shersingh7@users.noreply.github.com>
2021-10-30 09:11:26 -0400

    Initial commit

---
+++ main.py
@@ -0,0 +1,13 @@
+## Maths Basics
+
+# print(10 / 3) # For float
+# print(10 // 3) # For Integer
+# print(10 ** 3) # 10 to the power 3
+
+x = 2.9
+print(round(x)) # rounds the number
+print(abs(-2.9)) # Always returns positive number
+
+## Import "math" module for better math methods
+
+
